68edd58d6852d0af919bf3bf24ebc9cce6d603ce
Soulem <ewilcher@fullsail.edu>
2024-05-02 08:39:37 -0400

    Updated team service methods.
    Updated all team service methods to have some validation if needed.

--- PokemonTeamBuilder.API/Service/PKMTeamService.cs
+++ PokemonTeamBuilder.API/Service/PKMTeamService.cs
@@ -10,15 +10,12 @@ public class PKMTeamServices : IPKMTeamService
     private readonly IPKMTeamRepo _pkmTeamRepo;
     public PKMTeamServices(IPKMTeamRepo pkmTeamRepo) => _pkmTeamRepo = pkmTeamRepo;
 
-    public Task<PokemonTeam> CreateNewTeam(PokemonTeam team, int trainerID)
-    {
-        return _pkmTeamRepo.CreateNewTeam(team, trainerID);
-    }
+    public Task<PokemonTeam> CreateNewTeam(PokemonTeam pkmTeam, int trainerID) => 
+        DoesTeamExist(pkmTeam.Id).Result ? throw new ObjectExistException() : _pkmTeamRepo.CreateNewTeam(pkmTeam, trainerID);
 
-    public Task<PokemonTeam> DeleteTeam(int id)
-    {
-        return _pkmTeamRepo.DeleteTeam(id);
-    }
+    public Task<PokemonTeam> DeleteTeam(int id) => 
+        DoesTeamExist(id).Result ? _pkmTeamRepo.DeleteTeam(id) : throw new NullReferenceException();
+    
 
     public Task<IEnumerable<PokemonTeam?>> GetAll(int trainerID)
     {
@@ -26,20 +23,14 @@ public class PKMTeamServices : IPKMTeamService
         return returnList.Result.ToList().IsNullOrEmpty() ? throw new EmptyListException() : returnList;
     }
 
-    public Task<PokemonTeam> GetTeam(int id)
-    {
-        return _pkmTeamRepo.GetTeam(id);
-    }
+    public Task<PokemonTeam> GetTeam(int id) => 
+        DoesTeamExist(id).Result ? _pkmTeamRepo.GetTeam(id) : throw new NullReferenceException();
 
-    public Task<PokemonTeam> GetTeam(string name)
-    {
-        return _pkmTeamRepo.GetTeam(name);
-    }
+    public Task<PokemonTeam> GetTeam(string name) =>
+        DoesTeamExist(name).Result ? _pkmTeamRepo.GetTeam(name) : throw new BadNameException();
 
-    public Task<PokemonTeam> UpdateTeam(PokemonTeam pkmTeam)
-    {
-        return _pkmTeamRepo.UpdateTeam(pkmTeam);
-    }
+    public Task<PokemonTeam> UpdateTeam(PokemonTeam pkmTeam) => 
+        DoesTeamExist(pkmTeam.Id).Result ? _pkmTeamRepo.UpdateTeam(pkmTeam) : throw new NullReferenceException();
 
     private Task<bool> DoesTeamExist(string name) => _pkmTeamRepo.DoesTeamExist(name);
     private Task<bool> DoesTeamExist(int id) => _pkmTeamRepo.DoesTeamExist(id);
